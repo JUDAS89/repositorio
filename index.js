be53252e7766a18b48d6ef61dd64d919580ecd4f
JUDAS89 <lorena.peroza@outlook.com>
2024-06-08 07:44:51 -0400

    direccionando index.html con el GET (punto2)

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const path = require('path') //para funcionamiento del requerimiento2
 
+
+// 1_LEVANTAR SERVIDOR
 app.listen(3000,console.log("¡Servidor encendido!"))
 
-app.get("/home", (req, res) => {
-    res.send("Hello Express Js")
-})
+// 2_DEVOLVER UNA PAGINA WEB COMO RESPUESTA A UNA CONSULTA GET
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
+  });
+
