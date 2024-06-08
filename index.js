09e878218033502a5a52afcc7f73be02c1dd292b
JUDAS89 <lorena.peroza@outlook.com>
2024-06-08 06:51:50 -0400

    instalando nodemon

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000,console.log("¡Servidor encendido!"))
+
+app.get("/home", (req, res) => {
+    res.send("Hello lorena Express Js")
+})
